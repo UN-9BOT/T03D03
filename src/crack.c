39cfc25672b7cce95c3924eb6d1f1d07c00ea5d7
Yajaira Hercules <un.9bot@gmail.com>
2023-03-08 20:35:01 +0300

    fix 7

--- src/crack.c
+++ src/crack.c
@@ -3,8 +3,11 @@
 int main(void) {
     float x, y, res;
 
-    scanf("%f %f", &x, &y);
-    res = x * x + y * y;
-    (res < 25) ? printf("GOTCHA") : printf("MISS");
+    if ((scanf("%f %f", &x, &y) == 2)) {
+        res = x * x + y * y;
+        (res < 25) ? printf("GOTCHA") : printf("MISS");
+    } else {
+        printf("n/a");
+    }
     return (0);
 }
